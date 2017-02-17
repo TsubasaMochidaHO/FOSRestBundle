1bb5ee240b7afb0c14c72c199dee3fd060f9534f
Guilhem Niot <guilhem.niot@gmail.com>
2017-02-17 21:17:04 +0100

    [RequestBodyParamConverter] Force being explicit

--- Request/RequestBodyParamConverter.php
+++ Request/RequestBodyParamConverter.php
@@ -124,7 +124,7 @@ class RequestBodyParamConverter implements ParamConverterInterface
      */
     public function supports(ParamConverter $configuration)
     {
-        return null !== $configuration->getClass();
+        return null !== $configuration->getClass() && null !== $configuration->getConverter();
     }
 
     /**
